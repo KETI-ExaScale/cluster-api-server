a644decc1e9b028c728a4c594757175cea160d4e
hth0919 <hth0919@gmail.com>
2023-09-05 14:04:00 +0900

    cluster-api-server

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"cluster-api-server/pkg/rest"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+	"k8s.io/klog"
+)
+
+func main() {
+	router := httprouter.New()
+	rest.AddResource(router, new(rest.NodeMetricResource))
+	rest.AddResource(router, new(rest.NodeResource))
+	rest.AddResource(router, new(rest.ClusterResource))
+
+	klog.Fatal(http.ListenAndServe(":30850", router))
+}
